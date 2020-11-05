61ad3ceb6fd5401959b0974c62eb228d126b17aa
Iliyan-Y <ili.yankov91@gmail.com>
2020-11-05 16:14:26 +0000

    GREEN  Any dead cell with exactly 3 neighbours becomes a live cell

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -43,10 +43,10 @@ describe('Game', () => {
       ]);
       game.verifyPopulation();
       expect(game.population[0].alive).toBe(true);
-      expect(game.population[1].alive).toBe(true);
-      expect(game.population[2].alive).toBe(false);
+      expect(game.population[1].alive).toBe(false);
+      expect(game.population[2].alive).toBe(true);
       expect(game.population[3].alive).toBe(true);
-      expect(game.population[4].alive).toBe(false);
+      expect(game.population[4].alive).toBe(true);
     });
 
     it('Any live cell with 2 or 3 live neighbours survives until next tick', () => {
@@ -56,6 +56,20 @@ describe('Game', () => {
         new FakceCell(),
         new FakceCell(),
         new FakceCell(false),
+      ]);
+      game.verifyPopulation();
+      expect(game.population[0].alive).toBe(true);
+      expect(game.population[1].alive).toBe(true);
+      expect(game.population[2].alive).toBe(true);
+      expect(game.population[3].alive).toBe(true);
+    });
+
+    it('Any dead cell with exactly 3 neighbours becomes a live cell', () => {
+      game.addPopulation([
+        new FakceCell(),
+        new FakceCell(),
+        new FakceCell(false),
+        new FakceCell(),
       ]);
       game.verifyPopulation();
       expect(game.population[0].alive).toBe(true);
